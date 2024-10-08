cdb7bdf733e520e35f4ae8bf359b9a39c5364c05
maclean23 <122225257+maclean23@users.noreply.github.com>
2024-10-09 00:29:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-### vim main.tf
-
 resource "aws_instance" "test_instances" {
-  ami  = "ami-0ea3c35c5c3284d82"
-  count = "1"
-  instance_type = "t2.micro"
-  key_name = "du-devops"
+  ami             = "ami-0ea3c35c5c3284d82"
+  count           = 1
+  instance_type   = "t2.micro"
+  key_name        = "du-devops"
+
   user_data = <<-EOF
-                #! /bin/bash
-                sudo apt-get update
-                sudo git clone http://github.com/maclean23/Jenkins-server-setup
-                sudo chmod -R Jenkins-server-setup 
+                #!/bin/bash
+                sudo apt-get update -y
+                sudo apt-get install -y git
+                sudo git clone https://github.com/maclean23/Jenkins-server-setup
+                sudo chmod -R 755 Jenkins-server-setup
+                sudo chmod +x Jenkins-server-setup/ubuntu-setup-user.sh
                 sudo Jenkins-server-setup/ubuntu-setup-user.sh
         EOF
 
